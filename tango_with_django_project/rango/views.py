1779b8e6bda81a7787328ba0711a7d58ef405f2e
Majd Sehwail <2096507s@student.gla.ac.uk>
2015-03-08 23:16:58 +0000

    Chapter 15 Complete

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate, login,logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
+from rango.bing_search import run_query
 
 def index(request):
 
@@ -243,3 +244,17 @@ def user_logout(request):
 
     # Take the user back to the homepage.
     return HttpResponseRedirect('/rango/')
+
+
+def search(request):
+
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render(request, 'rango/search.html', {'result_list': result_list})
